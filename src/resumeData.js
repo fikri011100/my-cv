27acd606c8369c19aa25d1f50dc3ded1fd4a2bba
Muhammad Fikri Imaduddin Masyhuri <fikri.im@gmail.com>
2018-09-09 17:49:31 +0700

    .

---
+++ src/resumeData.js
@@ -0,0 +1,99 @@
+let resumeData = {
+  "imagebaseurl":"https://techonoeticsclub.github.io/",
+  "name": "Fikri Imaduddin",
+  "role": "Android Developer",
+  "emailid": " fikri.im@gmail.com",
+  "roleDescription": "I like dabbling in various parts of Mobile development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks":[
+      {
+        "name":"facebook",
+        "url":"http://facebook.com/fikri.imaduddinmasyhuri",
+        "className":"fa fa-facebook"
+      },
+      {
+        "name":"twitter",
+        "url":"http://twitter.com/ImaduddinFikri",
+        "className":"fa fa-twitter"
+      },
+      {
+        "name":"google-plus",
+        "url":"http://plus.google.com/111386982317087522474",
+        "className":"fa fa-google-plus"
+      },
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/muhammad-fikri-imaduddin-masyhuri-b62827136/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"http://github.com/fikri011100",
+        "className":"fa fa-github"
+      }
+    ],
+  "aboutme":"i am from East Java, and i'm 17, first time I learn about mobile development in 16",
+  "address":"Jl. Jembawan VIII 3H 14 Pakis, Kab Malang, East Java",
+  "website":"http://santriprogrammer.com ",
+  "education":[
+    {
+      "UniversityName":"SMK IDN",
+      "specialization":"Majors RPL(Mobile Development)",
+      "Achievements":""
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Qasir.id",
+      "specialization":"Android Developer",
+      "MonthOfLeaving":"Feb",
+      "YearOfLeaving":"2018"
+    },
+    {
+      "CompanyName":"PT. Cendana Teknika Utama",
+      "specialization":"Android Developer",
+      "MonthOfLeaving":"Jun",
+      "YearOfLeaving":"2018"
+    }
+  ],
+  "skillsDescription":"",
+  "skills":[
+    {
+      "skillname":"android"
+    },
+    {
+      "skillname":"ios"
+    },
+    {
+      "skillname":"javascript"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"Qasir.Id",
+      "description":"mobileapp",
+      "imgurl":"images/imgqasir.png"
+    },
+    {
+      "name":"Qasir Agent",
+      "description":"mobileapp",
+      "imgurl":"images/imgqasiragent.png"
+    },
+    {
+      "name":"Kios MD",
+      "description":"mobileapp",
+      "imgurl":"images/asd.png"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"Don't Stop when you're tired, stop when you're done",
+      "name":"Unknown"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
